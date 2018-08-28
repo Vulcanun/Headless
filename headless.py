6a65f21370ffa74ba8e946167e0f036b92233b5b
Lucas Pereira <vulcanun@hotmail.com>
2018-08-28 00:14:15 -0300

    new arguments
    Added -L and -A args for following redirects and using custom user-agent values, respectively,
    Also added exception handling after the main request.

--- headless.py
+++ headless.py
@@ -5,12 +5,20 @@ headers = {'Strict-Transport-Security', 'Public-Key-Pins', 'X-Frame-Options', 'X
 
 parser = argparse.ArgumentParser(description='Fetch HTTP headers related to security.')
 parser.add_argument('target', help='Target server.')
+parser.add_argument('-L', '--location', action='store_true', dest="location", help='If set, headless will follow 30X redirects. Default is False.', default=False)
+parser.add_argument('-A', '--user-agent', dest="uagent", help='Allows for the usage of custom User-Agent value.', default=requests.Session().headers['User-Agent'])
 args = parser.parse_args()
 
-r = requests.head(args.target).headers
+try:
+    r = requests.head(args.target, allow_redirects=args.location, headers={'User-Agent':args.uagent})
 
-for h in headers:
-    try:
-        print "\033[92m[+] The '" + h + "' header was detected with the '" + r[h] + "' value."
-    except Exception as e:
-        print "\033[91m[-] The '" + h + "' header was not detected."
+    print '\033[92m[+] Request Completed, received code ' + str(r.status_code) + ' for "' + r.url + '".\n'
+
+    for h in headers:
+        try:
+            print "\033[92m[+] The '" + h + "' header was detected with the '" + r.headers[h] + "' value."
+        except Exception as e:
+            print "\033[91m[-] The '" + h + "' header was not detectedself."
+except Exception as e:
+    print 'Uh oh, something bad happened. Dying.'
+    print 'Exception: ' + e
